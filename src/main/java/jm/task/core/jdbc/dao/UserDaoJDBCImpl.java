10ead4bfd0cd592dd2fe6774298d9f16842510a4
unknown <dementev438@gmail.com>
2022-10-05 16:17:07 +0700

    1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,88 +8,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private final Connection connection = Util.getConnection();
-
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS new_schema.users" +
-                    "(id mediumint not null auto_increment," +
-                    " name VARCHAR(50), " +
-                    "lastname VARCHAR(50), " +
-                    "age tinyint, " +
-                    "PRIMARY KEY (id))");
-            System.out.println("Таблица создана");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public void dropUsersTable() {
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("Drop table if exists mybasetest.users");
-            System.out.println("Таблица удалена");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String sql = "INSERT INTO new_schema.users(name, lastname, age) VALUES(?,?,?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-            preparedStatement.executeUpdate();
-            System.out.println("User с именем – " + name + " добавлен в базу данных");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public void removeUserById(long id) {
-        String sql = "DELETE FROM new_schema.users where id";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.executeUpdate(sql);
-            System.out.println("User удален");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public List<User> getAllUsers() {
-        List<User> allUser = new ArrayList<>();
-        String sql = "SELECT id, name, lastName, age from new_schema.users";
-
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            ResultSet resultSet = preparedStatement.executeQuery(sql);
-
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setLastName(resultSet.getString("lastName"));
-                user.setAge(resultSet.getByte("age"));
-                allUser.add(user);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return allUser;
+        return null;
     }
 
     public void cleanUsersTable() {
-        String sql = "DELETE FROM new_schema.users";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.executeUpdate(sql);
-            System.out.println("Таблица очищена");
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Не удалось очистить");
-        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,10 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,17 +14,29 @@ public class Util {
     private static final String HOST = "jdbc:mysql://localhost:3306/new_schema?serverTimezone=Europe/Moscow";
     private static final String LOGIN = "root";
     private static final String PASSWORD = "root";
+    private static SessionFactory sessionFactory = null;
 
-    public static Connection getConnection() {
-        Connection connection = null;
+    public static SessionFactory getConnection() {
+
         try {
-            Class.forName(DRIVER);
-            connection = DriverManager.getConnection(HOST, LOGIN, PASSWORD);
-            System.out.println("ok");
-        } catch (SQLException | ClassNotFoundException e) {
+            Configuration configuration = new Configuration()
+                    .setProperty("hibernate.connection.driver_class", DRIVER)
+                    .setProperty("hibernate.connection.url", HOST)
+                    .setProperty("hibernate.connection.username", LOGIN)
+                    .setProperty("hibernate.connection.password", PASSWORD)
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable e) {
             e.printStackTrace();
         }
-
-        return connection;
+        return sessionFactory;
+    }
+    public static void closeConnection() {
+        if (sessionFactory != null)
+            sessionFactory.close();
     }
 }
